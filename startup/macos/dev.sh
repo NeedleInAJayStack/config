81454bc9daabcdd60c60293885fffc50dd6baff4
Jay Herron <NeedleInAJayStack@protonmail.com>
2024-05-25 16:44:10 -0700

    feature: Adds pyenv

--- startup/macos/dev.sh
+++ startup/macos/dev.sh
@@ -12,3 +12,20 @@ else
     echo 'if which swiftenv > /dev/null; then eval "$(swiftenv init -)"; fi' >> ~/.zshrc
     echo 'if which swiftenv > /dev/null; status --is-interactive; and source (swiftenv init -|psub); end' >> ~/.config/fish/config.fish
 fi
+
+# pyenv
+if brew list pyenv > /dev/null;
+then
+    echo "pyenv already installed"
+else
+    brew install pyenv
+
+    echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.bash_profile
+    echo 'eval "$(pyenv init -)"' >> ~/.bash_profile
+
+    echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.zshrc
+    echo 'eval "$(pyenv init -)"' >> ~/.zshrc
+    
+    fish -c "set -Ux PYENV_ROOT $HOME/.pyenv"
+    echo 'if which pyenv > /dev/null; status --is-interactive; and source (pyenv init -|psub); end' >> ~/.config/fish/config.fish
+fi
